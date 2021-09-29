5d8b04e6d5a426436b53cc4c6d5fc027bfdad37a
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-09-29 20:27:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -50,7 +50,7 @@
         I am not sure if I am going to continue this project because it just seems a little insane to do all this
         just so that I can raytrace some (at best) lambaritan spheres at ~30fps.
 
-        Also multi-threaded mod does not seem to render anything on my graphics card. But it seems to work fine
+        Also the multi-threaded mode does not seem to render anything on my graphics card. But it seems to work fine
         on my integrated Intel Iris XE. ¯\_(ツ)_/¯ not that multi-threading a real-time raytracer has much benefit
         anyway.
 
